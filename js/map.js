006792ff15a9e8667e819a8d9ed05fef38778cbb
Keith Hamm <hkhamm@gmail.com>
2015-10-18 12:40:22 -0700

    working map with pins that have deletable popups

---
+++ js/map.js
@@ -0,0 +1,26 @@
+var mapapp = {};
+
+mapapp.map = L.map('map').setView([44.049, -123.095], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'hkhamm.cifwsgusf3biduekqq20slwfn',
+    accessToken: 'pk.eyJ1IjoiaGtoYW1tIiwiYSI6ImNpZndzZ3c4dzM3cmx1ZGx5MzIyNnU5YXkifQ.RzTbznkqSh_oZrOY64dBdA'
+}).addTo(mapapp.map);
+
+mapapp.map.on('click', function(e) {
+    var marker = L.marker();
+    marker.setLatLng(e.latlng)
+          .on('popupopen', function() {
+                var tmpMarker = this;
+                $(".marker-delete-button:visible").click(function() {
+                    mapapp.map.removeLayer(tmpMarker);
+                });
+            })
+          .bindPopup(e.latlng.toString() + "<br><div class='popup'>" +
+                     "<input type='button' value='Delete' class='marker-delete-button'/></div>")
+          .addTo(mapapp.map);
+});
